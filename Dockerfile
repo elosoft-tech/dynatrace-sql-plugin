d24aa5197946c522f0dcf769a9c05c28d665588e
Fernando L Albuquerque <fernando@elosoft.com.br>
2022-02-10 15:51:33 -0300

    Save changes ...

--- Dockerfile
+++ Dockerfile
@@ -7,9 +7,9 @@ COPY installODBCDrivers.sh ./
 
 ENV LD_LIBRARY_PATH=/opt/oracleodbc/instantclient_19_8:/opt/microsoft/msodbcsql17/lib64
 ENV ODBCINI=/etc/odbc.ini
-ENV BUILD_PACKAGES="unixodbc-dev python-dev curl wget python-pip unzip libffi-dev"
+ENV BUILD_PACKAGES="unixodbc-dev python3-dev curl wget python3-pip unzip libffi-dev libodbc1"
 RUN bash ./installODBCDrivers.sh \
-    && pip install  -r requirements.txt \
+    && pip3 install -r requirements.txt \
     && apt-get remove --purge -y $BUILD_PACKAGES && apt-get clean autoclean && apt-get autoremove --yes && rm -rf /var/lib/apt/lists/*
 COPY src/ .
 
--- installODBCDrivers.sh
+++ installODBCDrivers.sh
@@ -1,12 +1,14 @@
 apt update
-apt install -y unixodbc unixodbc-dev python-dev libgssapi-krb5-2 odbc-postgresql curl wget python-pip libaio1 libffi-dev unzip
+apt install -y unixodbc unixodbc-dev python-dev libgssapi-krb5-2 odbc-postgresql curl wget python3-pip libaio1 libffi-dev unzip libodbc1
 
+echo "======= MYSQL ========"
 #MySQL
-wget https://dev.mysql.com/get/Downloads/Connector-ODBC/8.0/mysql-connector-odbc_8.0.21-1debian10_amd64.deb
-dpkg -i mysql-connector-odbc_8.0.21-1debian10_amd64.deb
-rm mysql-connector-odbc_8.0.21-1debian10_amd64.deb
+wget https://downloads.mysql.com/archives/get/p/10/file/mysql-connector-odbc_8.0.21-1ubuntu20.04_amd64.deb
+dpkg -i mysql-connector-odbc_8.0.21-1ubuntu20.04_amd64.deb
+rm mysql-connector-odbc_8.0.21-1ubuntu20.04_amd64.deb
 
 #MSSQL
+echo "======= MSSQL ========"
 curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
 curl https://packages.microsoft.com/config/debian/10/prod.list > /etc/apt/sources.list.d/mssql-release.list
 
@@ -14,9 +16,11 @@ apt update
 ACCEPT_EULA=Y apt install msodbcsql17
 apt install libgssapi-krb5-2
 
+echo "======= ORACLE ========"
 #Oracle
 wget https://download.oracle.com/otn_software/linux/instantclient/19800/instantclient-basiclite-linux.x64-19.8.0.0.0dbru.zip
 wget https://download.oracle.com/otn_software/linux/instantclient/19800/instantclient-odbc-linux.x64-19.8.0.0.0dbru.zip
+rm -rf /opt/oracleodbc 
 mkdir /opt/oracleodbc
 unzip instantclient-basiclite-linux.x64-19.8.0.0.0dbru.zip -d /opt/oracleodbc
 unzip instantclient-odbc-linux.x64-19.8.0.0.0dbru.zip -d /opt/oracleodbc
